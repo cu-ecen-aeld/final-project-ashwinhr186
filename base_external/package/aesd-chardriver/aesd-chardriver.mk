0ae10c86090eede1eab9f535ae671feac011d4b6
ashwinhr186 <ashwinhr186@Ubuntu.myguest.virtualbox.org>
2023-11-01 00:25:10 -0600

    ran save-config and build scripts once again

--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -5,7 +5,7 @@ AESD_CHARDRIVER_VERSION = a5faa568666e1b2063b7bbaab310912ad73fcb7e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system. 
 # Your site should start with git@github.com:
-AESD_CHARDRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-ashwinhr186.git
+AESD_CHARDRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ashwinhr186.git'
 AESD_CHARDRIVER_SITE_METHOD = git
 AESD_CHARDRIVER_GIT_SUBMODULES = YES
 
