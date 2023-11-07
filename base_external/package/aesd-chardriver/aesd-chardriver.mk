3ef7ca3d4dbda8e69474e116bccd28ef07c0d27a
ashwinhr186 <ashwinhr186@Ubuntu.myguest.virtualbox.org>
2023-11-07 12:59:57 -0700

    updated a3 commit hash, full tests works locally

--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -1,7 +1,7 @@
 #AESD_CHARDRIVER
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHARDRIVER_VERSION = d8857a60eba611e66d92df49aec4db66d771dfc7
+AESD_CHARDRIVER_VERSION = 067563356e70cf3a17927e9d1d5d51d436314ae7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system. 
 # Your site should start with git@github.com:
